48623f68ec5601a419701822e315a2c67111cd4f
Olek <aleksandermoscicki@protonmail.com>
2022-07-09 15:38:19 +0200

    update mobile version

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,22 +1,19 @@
 import React from "react";
 import "../styles/Footer.css";
-import {
-  AiOutlineInstagram,
-  AiOutlineMail,
-  AiOutlineCopyrightCircle,
-} from "react-icons/ai";
+import { AiOutlineInstagram, AiOutlineMail } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <footer>
       <div className="footer-item">
-        <AiOutlineInstagram />
-        <AiOutlineMail />
+        <div className="footer-icons">
+          <AiOutlineInstagram />
+          <AiOutlineMail />
+        </div>
         <p>Święty Marcin 49C Poznań, Polska | 555-555-555</p>
       </div>
       <div className="footer-item">
-        <AiOutlineCopyrightCircle />
-        <p>Inky tattoo studio 2022, All rights reserved</p>
+        <p>© Inky tattoo studio 2022, All rights reserved</p>
       </div>
     </footer>
   );
